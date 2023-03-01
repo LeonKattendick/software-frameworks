892aa1b9c053e8ad20e868d4519d9fd3379d5ec0
LeonKattendick <leon.kattendick@gmail.com>
2023-03-01 20:04:51 +0100

    kafka

---
+++ crawler-module/src/main/java/at/technikum/crawler/Crawler.java
@@ -0,0 +1,33 @@
+package at.technikum.crawler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+@Slf4j
+public class Crawler {
+
+    public static void main(String[] args) {
+        log.info("Starting Kafka Producer...");
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        try (KafkaProducer<String, String> producer = new KafkaProducer<>(properties)) {
+
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world");
+
+            producer.send(producerRecord);
+            producer.flush();
+
+        } catch (Exception e) {
+            log.error("Error starting Kafka producer", e);
+        }
+    }
+}
---
+++ kafka-module/src/main/java/at/technikum/kafka/Kafka.java
@@ -0,0 +1,43 @@
+package at.technikum.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+@Slf4j
+public class Kafka {
+
+    public static void main(String[] args) {
+        log.info("Starting Kafka Consumer...");
+
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "test");
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties)) {
+
+            consumer.subscribe(Collections.singletonList("demo_java"));
+
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+                for (ConsumerRecord<String, String> record : records) {
+                    log.info("Key: " + record.key() + ", Value: " + record.value());
+                    log.info("Partition: " + record.partition() + ", Offset:" + record.offset());
+                }
+            }
+        } catch (Exception e) {
+            log.error("Error starting Kafka consumer", e);
+        }
+    }
+}

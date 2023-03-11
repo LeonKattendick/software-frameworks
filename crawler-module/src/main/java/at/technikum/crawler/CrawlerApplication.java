afbb8cf86cd563ae87ea34827eac99a2f9a4d397
LeonKattendick <leon.kattendick@gmail.com>
2023-03-11 17:32:27 +0100

    docker fix

--- crawler-module/src/main/java/at/technikum/crawler/CrawlerApplication.java
+++ crawler-module/src/main/java/at/technikum/crawler/CrawlerApplication.java
@@ -2,10 +2,8 @@ package at.technikum.crawler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
-@EnableKafka
 public class CrawlerApplication {
 
     public static void main(String[] args) {

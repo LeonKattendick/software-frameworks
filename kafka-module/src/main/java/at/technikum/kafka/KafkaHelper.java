421165ed068da285e6a1286a412484e8a1f39cf0
franz <franz.schillinger@gmx.at>
2023-03-16 11:22:26 +0100

    KafkaStreams v0.3

--- kafka-module/src/main/java/at/technikum/kafka/KafkaHelper.java
+++ kafka-module/src/main/java/at/technikum/kafka/KafkaHelper.java
@@ -1,22 +1,25 @@
 package at.technikum.kafka;
 
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.streams.StreamsConfig;
 
 import java.util.Properties;
+
 
 public class KafkaHelper {
     public static Properties propertiesConfig() {
         Properties properties = new Properties();
         properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-streams-application");
         properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
 
-        /*
+        properties.put("schema.registry.url", "http://localhost:8081");
+
+
+        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String());
+        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String());
+/*
+
         application.id=kafka-streams-101
         bootstrap.servers=localhost:29092
         input.topic.name=random-strings
--- kafka-module/src/main/java/at/technikum/kafka/KafkaStreamProcessor.java
+++ kafka-module/src/main/java/at/technikum/kafka/KafkaStreamProcessor.java
@@ -1,24 +1,96 @@
 package at.technikum.kafka;
 
-import org.apache.kafka.streams.*;
+import at.technikum.commons.Constants;
+import at.technikum.commons.schema.dota.Dota2Player;
+import at.technikum.commons.schema.league.LeagueOfLegendsPlayer;
+import at.technikum.commons.schema.unified.UnifiedPlayer;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Produced;
 
+import java.util.Collections;
 import java.util.Properties;
-import java.util.concurrent.CountDownLatch;
+
 
 public class KafkaStreamProcessor {
 
     public static void main(String[] args) {
+
+        // Config -- WORKING
+
         final StreamsBuilder builder = new StreamsBuilder();
         final Properties props = KafkaHelper.propertiesConfig(); //TODO props
 
 
-        // TODO: streams
-        builder.stream("tracker")
-                .to("newstream");
+        // Serdes
+        final Serde<String> stringSerde = Serdes.String();
+        final Serde<Dota2Player> key1SpecificAvroSerde = new SpecificAvroSerde<>();
+        key1SpecificAvroSerde.configure(Collections.singletonMap("schema.registry.url",
+                "http://localhost:8081"), false);
+
+        final Serde<LeagueOfLegendsPlayer> key2SpecificAvroSerde = new SpecificAvroSerde<>();
+        key2SpecificAvroSerde.configure(Collections.singletonMap("schema.registry.url",
+                "http://localhost:8081"), true);
+
+        final Serde<UnifiedPlayer> valueSpecificAvroSerde = new SpecificAvroSerde<>();
+        valueSpecificAvroSerde.configure(Collections.singletonMap("schema.registry.url",
+                "http://localhost:8081"), false);
+
+
+        // Streams
+
+        //builder.stream(Constants.TOPIC_NAME_DOTA2)                .to(Constants.TOPIC_NAME_UNIFIED);
+
+        /*
+        KStream<String, UnifiedPlayer> dotaToUnifiedPlayerStream =
+                builder.stream(Constants.TOPIC_NAME_DOTA2);
+        dotaToUnifiedPlayerStream.to(Constants.TOPIC_NAME_UNIFIED,
+                Produced.with(stringSerde, valueSpecificAvroSerde));
+
+
+         */
+
+        KStream<String, UnifiedPlayer> dotaToUnifiedPlayerStream =
+                builder.stream(Constants.TOPIC_NAME_DOTA2,
+                        Consumed.with(stringSerde, valueSpecificAvroSerde));
+        dotaToUnifiedPlayerStream.to(Constants.TOPIC_NAME_UNIFIED,
+                Produced.with(stringSerde, valueSpecificAvroSerde));
+
+        /*
+
+        KStream<LeagueOfLegendsPlayer, UnifiedPlayer> leagueToUnifiedPlayerStream =
+                builder.stream(Constants.TOPIC_NAME_UNIFIED,
+                        Consumed.with(key2SpecificAvroSerde, valueSpecificAvroSerde));
+
+        leagueToUnifiedPlayerStream.to(Constants.TOPIC_NAME_UNIFIED,
+                Produced.with(key2SpecificAvroSerde, valueSpecificAvroSerde));
+        */
+
+        // Start Stream -- WORKING
+
+        final KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        streams.cleanUp();
+        streams.start();
+
+
+        /*
+        *
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                streams.close();
+            }
+        }));
 
 
         final Topology topology = builder.build();
-        final KafkaStreams streams = new KafkaStreams(topology, props);
         final CountDownLatch latch = new CountDownLatch(1);
+        *
+        * */
     }
 }
